2ea7b1714703d673cefbdcf4a3eb902141679e16
Chris Cankov <cankov.chris@gmail.com>
2017-04-17 14:06:46 -0700

    complete user_controller

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,9 +1,51 @@
 class UsersController < ApplicationController
   def index
-    render text: "I'm in the index action!"
+    users = User.all
+    render json: users
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
   end
 
   def create
-    render json: params
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render(
+        json: user.errors.full_messages, status: :unprocessable_entity
+      )
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      redirect_to user_url(user)
+    else
+      render(
+        json: user.errors.full_messages, status: :unprocessable_entity
+      )
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user.destroy
+      render json: user
+    else
+      render(
+        json: user.errors.full_messages, status: :unprocessable_entity
+      )
+    end
+  end
+
+
+  private
+
+  def user_params
+    params[:user].permit(:name, :email)
   end
 end
